5702b5c34744d16a20e3573178fa21d5e11058bc
최대현 <eogusalrns@naver.com>
2021-04-05 16:13:38 +0900

    영화 유튜브 네이버 open api 04_05 7교시 커밋

--- WPFMiniProject/NaverMovieFinderApp/MainWindow.xaml.cs
+++ WPFMiniProject/NaverMovieFinderApp/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 using NaverMovieFinderApp.Model;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -36,6 +38,7 @@ namespace NaverMovieFinderApp
             if (string.IsNullOrEmpty(TxtMovieName.Text))
             {
                 StsResult.Content = "검색할 영화명을 입력 후, 검색버튼을 눌러주세요.";
+                Commons.ShowMessageAsync("검색", "검색할 영화명을 입력 후, 검색버튼을 눌러주세요.");
                 return;
             }
 
@@ -43,13 +46,15 @@ namespace NaverMovieFinderApp
             try
             {
                 ProcSearchNaverApi(TxtMovieName.Text);
+                Commons.ShowMessageAsync("검색", "영화검색 완료");
+                
             }
             catch (Exception ex)
             {
                 Commons.ShowMessageAsync("예외", $"예외발생 : {ex}");
                 Commons.LOGGER.Error($"예외발생 : {ex}");
             }
-            
+            Commons.IsFavorite = false; //즐겨찾기 아님
         }
 
         private void ProcSearchNaverApi(string movieName)
@@ -87,6 +92,26 @@ namespace NaverMovieFinderApp
 
             this.DataContext = movieItems;
 
+            //List<NaverFavoriteMovies> list = new List<NaverFavoriteMovies>();
+
+            //foreach (MovieItem item in GrdData.SelectedItems)
+            //{
+            //    NaverFavoriteMovies temp = new NaverFavoriteMovies()
+            //    {
+            //        Title = item.Title,
+            //        Link = item.Link,
+            //        Image = item.Image,
+            //        SubTitle = item.SubTitle,
+            //        PubDate = item.PubDate,
+            //        Director = item.Director,
+            //        Actor = item.Actor,
+            //        UserRating = item.UserRating,
+            //        RegDate = DateTime.Now
+            //    };
+
+            //    list.Add(temp);
+            //}
+
         }
 
         private void TxtMovieName_KeyDown(object sender, KeyEventArgs e)
@@ -103,13 +128,26 @@ namespace NaverMovieFinderApp
             //    return;
             //}
 
+            if (GrdData.SelectedItem is NaverFavoriteMovies)
+            {
+                var movie = GrdData.SelectedItem as NaverFavoriteMovies;
+                //Commons.ShowMessageAsync("결과", $"{movie.Image}");
+                if (string.IsNullOrEmpty(movie.Image))
+                {
+                    ImgPoster.Source = new BitmapImage(new Uri("No_Image.jpg",UriKind.RelativeOrAbsolute));
+                }
+                else
+                {
+                    ImgPoster.Source = new BitmapImage(new Uri(movie.Image, UriKind.RelativeOrAbsolute));
+                }
+            }
             if (GrdData.SelectedItem is MovieItem)
             {
                 var movie = GrdData.SelectedItem as MovieItem;
                 //Commons.ShowMessageAsync("결과", $"{movie.Image}");
                 if (string.IsNullOrEmpty(movie.Image))
                 {
-                    ImgPoster.Source = new BitmapImage(new Uri("No_Image.jpg",UriKind.RelativeOrAbsolute));
+                    ImgPoster.Source = new BitmapImage(new Uri("No_Image.jpg", UriKind.RelativeOrAbsolute));
                 }
                 else
                 {
@@ -123,6 +161,12 @@ namespace NaverMovieFinderApp
             if (GrdData.SelectedItems.Count == 0)
             {
                 Commons.ShowMessageAsync("오류", "즐겨찾기에 추가할 영화를 선택하세요(복수선택 가능)");
+                return;
+            }
+
+            if (Commons.IsFavorite == true)
+            {
+                Commons.ShowMessageAsync("즐겨찾기", "즐겨찾기 조회내용을 다시 즐겨찾기할 수 없습니다.");
                 return;
             }
 
@@ -150,6 +194,7 @@ namespace NaverMovieFinderApp
             {
                 using (var ctx = new OpenApiLabEntities())
                 {
+                    //ctx.NaverFavoriteMovies.Add(list); 아래코드와동일
                     ctx.Set<NaverFavoriteMovies>().AddRange(list);
                     ctx.SaveChanges();
                 }
@@ -161,6 +206,7 @@ namespace NaverMovieFinderApp
                 Commons.LOGGER.Error($"예외발생 : {ex}");
             }
             //GrdData.SelectedItem = null;
+            ImgPoster.Source = new BitmapImage(new Uri("No_Image.jpg", UriKind.RelativeOrAbsolute));
         }
 
         private void BtnViewWatchList_Click(object sender, RoutedEventArgs e)
@@ -168,7 +214,7 @@ namespace NaverMovieFinderApp
             this.DataContext = null;
             TxtMovieName.Text = "";
 
-            List<MovieItem> ListData = new List<MovieItem>();
+            //List<MovieItem> ListData = new List<MovieItem>();
             List<NaverFavoriteMovies> list = new List<NaverFavoriteMovies>();
 
             try
@@ -177,42 +223,143 @@ namespace NaverMovieFinderApp
                 {
                     list = ctx.NaverFavoriteMovies.ToList();
                 }
+                this.DataContext = list;
+                StsResult.Content = $"즐겨찾기 {list.Count}개 조회";
+                if (Commons.IsDelete)
+                    Commons.ShowMessageAsync("즐겨찾기", "즐겨찾기 삭제 완료");
+                else
+                    Commons.ShowMessageAsync("즐겨찾기", "즐겨찾기 조회 완료");
+
+                Commons.IsFavorite = true; // 즐겨찾기 맞음
             }
             catch (Exception ex)
             {
                 Commons.ShowMessageAsync("오류", $"오류내용 : {ex}");
                 Commons.LOGGER.Error($"예외발생 : {ex}");
+                Commons.IsFavorite = false; // 한번 더 명시적으로 처리
             }
+            Commons.IsDelete = false;
+            ImgPoster.Source = new BitmapImage(new Uri("No_Image.jpg", UriKind.RelativeOrAbsolute));
 
-            foreach (var item in list)
-            {
-                ListData.Add(new MovieItem(
-                    item.Title,
-                    item.Link,
-                    item.Image,
-                    item.SubTitle,
-                    item.PubDate,
-                    item.Director,
-                    item.Actor,
-                    item.UserRating
-                    ));
-            }
-            this.DataContext = ListData;
+            //변환필요없음
+            //foreach (var item in list)
+            //{
+            //    ListData.Add(new MovieItem(
+            //        item.Title,
+            //        item.Link,
+            //        item.Image,
+            //        item.SubTitle,
+            //        item.PubDate,
+            //        item.Director,
+            //        item.Actor,
+            //        item.UserRating
+            //        ));
+            //}
+            //this.DataContext = ListData;
+            //StsResult.Content = $"즐겨찾기 {ListData.Count}개 조회";
+            //Commons.ShowMessageAsync("즐겨찾기", "즐겨찾기 조회 완료");
+            //Commons.IsFavorite = true; // 즐겨찾기 맞음
         }
 
         private void BtnWatchTrailer_Click(object sender, RoutedEventArgs e)
         {
+            if (GrdData.SelectedItems.Count == 0)
+            {
+                Commons.ShowMessageAsync("유튜브영화", "영화를 선택하세요");
+                return;
+            }
 
+            if (GrdData.SelectedItems.Count > 1)
+            {
+                Commons.ShowMessageAsync("유튜브영화", "영화를 하나만 선택하세요");
+                return;
+            }
+
+            string movieName = "";
+
+            if (Commons.IsFavorite)//즐겨찾기
+            {
+                var item = GrdData.SelectedItem as NaverFavoriteMovies;
+                movieName = item.Title;
+            }
+            else
+            {
+                var item = GrdData.SelectedItem as MovieItem;
+                movieName = item.Title;
+            }
+
+            var trailerWindow = new TrailerWindow(movieName);
+            trailerWindow.Owner = this;
+            trailerWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            trailerWindow.ShowDialog();
         }
 
         private void BtnDeleteWatchList_Click(object sender, RoutedEventArgs e)
         {
+            if (Commons.IsFavorite == false)
+            {
+                Commons.ShowMessageAsync("즐겨찾기", "즐겨찾기 내용이 아니면 삭제할 수 없습니다");
+                return;
+            }
+            if(GrdData.SelectedItems.Count == 0)
+            {
+                Commons.ShowMessageAsync("즐겨찾기", "삭제할 즐겨찾기 내용을 선택하세요");
+                return;
+            }
 
+            //List<NaverFavoriteMovies> removelist = new List<NaverFavoriteMovies>();
+            foreach (NaverFavoriteMovies item in GrdData.SelectedItems)
+            {
+                //removelist.Add(item);
+                using (var ctx = new OpenApiLabEntities())
+                {
+                    //ctx.NaverFavoriteMovies.Remove(item);
+                    var itemDelete = ctx.NaverFavoriteMovies.Find(item.Idx);//삭제할 영화 객체 검색 후 생성
+                    ctx.Entry(itemDelete).State = EntityState.Deleted;// 검색객체 상태를 삭제로 변경
+                    ctx.SaveChanges();//commit
+                }
+            }
+            //조회쿼리 다시
+            BtnViewWatchList_Click(sender, e);
+            ImgPoster.Source = new BitmapImage(new Uri("No_Image.jpg", UriKind.RelativeOrAbsolute));
         }
 
         private void BtnNaverWatchTrailer_Click(object sender, RoutedEventArgs e)
         {
+            if (GrdData.SelectedItems.Count == 0)
+            {
+                Commons.ShowMessageAsync("네이버영화", "영화를 선택하세요");
+                return;
+            }
 
+            if (GrdData.SelectedItems.Count > 1)
+            {
+                Commons.ShowMessageAsync("네이버영화", "영화를 하나만 선택하세요");
+                return;
+            }
+
+            //선택된 영화 네이버영화 URL 가져오기
+            string linkUrl = "";
+            if (Commons.IsFavorite)//즐겨찾기
+            {
+                var item = GrdData.SelectedItem as NaverFavoriteMovies;
+                linkUrl = item.Link;
+            }
+            else
+            {
+                var item = GrdData.SelectedItem as MovieItem;
+                linkUrl = item.Link;
+            }
+            Process.Start(linkUrl); // 웹 브라우저 띄우기
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"즐겨찾기 여부 : {Commons.IsFavorite}");
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        { 
         }
     }
 }
---
+++ WPFMiniProject/NaverMovieFinderApp/TrailerWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using MahApps.Metro.Controls;
+using NaverMovieFinderApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NaverMovieFinderApp
+{
+    /// <summary>
+    /// TrailerWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class TrailerWindow : MetroWindow
+    {
+        List<YoutubeItem> youtubes; // youtube api 검색 결과담음
+        public TrailerWindow()
+        {
+            InitializeComponent();
+        }
+
+        public TrailerWindow(string movieName) : this()
+        {
+            LblMovieName.Content = $"{movieName} 예고편";
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            youtubes = new List<YoutubeItem>();//초기화
+            // 유튜브 API로 검색
+            //MessageBox.Show("유튜브 검색!");
+            ProcSearchYoutubeApi();
+        }
+
+        private async void ProcSearchYoutubeApi()
+        {
+            await LoadDataCollection();
+            LsvYoutubeSearch.ItemsSource = youtubes;
+        }
+
+        private async Task LoadDataCollection()
+        {
+            var youtubeService = new YouTubeService(
+                new BaseClientService.Initializer()
+                {
+                    ApiKey = "AIzaSyD3vOLfJ0Tiv0FBfQGg8RulTE9HSajf0hQ",
+                    ApplicationName = this.GetType().ToString()
+                });
+
+            var request = youtubeService.Search.List("snippet");
+            request.Q = LblMovieName.Content.ToString();//영화이름 예고편
+            request.MaxResults = 20; // 사이즈가 크면 멈춤 
+
+            var response = await request.ExecuteAsync();//검색시작 youtube openapi 호출
+
+            foreach (var item in response.Items)
+            {
+                if (item.Id.Kind.Equals("youtube#video"))
+                {
+                    YoutubeItem youtube = new YoutubeItem()
+                    {
+                        Title = item.Snippet.Title,
+                        Author = item.Snippet.ChannelTitle,
+                        URL = $"https://www.youtube.com/watch?v={item.Id.VideoId}"
+                    };
+                    //썸네일 이미지
+                    //Process.Start(youtube.URL);
+                    //byte[] imgBytes = new WebClient().DownloadData(item.Snippet.Thumbnails.Default__.Url);
+                    //using ( var ms = new MemoryStream(imgBytes))
+                    //{
+                    //    var source = new BitmapImage();
+                    //    source.BeginInit();
+                    //    source.StreamSource = ms;
+                    //    source.EndInit();
+                    //    //할당
+                    //    youtube.Thumbnail = new Image() { Source = source };
+                    //}
+                    youtube.Thumbnail = new BitmapImage(new Uri(item.Snippet.Thumbnails.Default__.Url,UriKind.RelativeOrAbsolute));
+
+                    youtubes.Add(youtube);
+                    
+                }
+            }
+        }
+
+        private async void LsvYoutubeSearch_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (LsvYoutubeSearch.SelectedItems.Count == 0)
+            {
+                Commons.ShowMessageAsync("유튜브보기", "예고편을 볼 영화를 선택하세요");
+                return;
+            }
+            if (LsvYoutubeSearch.SelectedItems.Count > 1)
+            {
+                Commons.ShowMessageAsync("유튜브보기", "예고편을 한 편만 선택하세요");
+                return;
+            }
+            if(LsvYoutubeSearch.SelectedValue is YoutubeItem)
+            {
+                var video = LsvYoutubeSearch.SelectedItem as YoutubeItem;
+                BrwYoutubeWatch.Source = new Uri(video.URL,UriKind.RelativeOrAbsolute);
+                //Process.Start(video.URL);
+            }
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            BrwYoutubeWatch.Source = null;//해제
+            BrwYoutubeWatch.Dispose();//리소스 즉시해제
+        }
+    }
+}
